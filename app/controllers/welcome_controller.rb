ab9cef3815ba3b52ebc1718da0865dec3b755891
alvin-li <lx3li@edu.uwaterloo.ca>
2020-03-09 20:12:38 -0400

    Fix Typo

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @url = request.url.partition("/?=").last.gsub('+', ' ').gsub(/[o|O]/, 'OMEGALUL')
+    @url = request.url.partition("/?=").last.gsub('+', ' ').gsub(/[o|O]/, ' OMEGALUL ')
     render :json => { "message": @url }
   end
 end
