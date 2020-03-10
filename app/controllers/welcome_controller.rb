41b16cd413a0703c3501f30a0de8fb4f9bcd1a08
alvin-li <lx3li@edu.uwaterloo.ca>
2020-03-09 20:10:44 -0400

    Setup Index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,6 @@
 class WelcomeController < ApplicationController
+  def index
+    @url = request.url.partition("/?=").last.gsub('+', ' ').gsub(/[o|O]/, 'OMEGALUL')
+    render :json => { "message": @url }
+  end
 end
