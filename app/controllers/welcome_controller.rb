a9d2366a8b13825e5267c587c91f35f20d295e5b
alvin-li <lx3li@edu.uwaterloo.ca>
2020-03-10 22:02:39 -0400

    Deglobalize url

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @url = request.url.partition("/?=").last.gsub('+', ' ').gsub(/[o|O]/, ' OMEGALUL ')
-    render :json => { "message": @url }
+    url = request.url.partition("/?=").last.gsub('+', ' ').gsub(/[o|O]/, ' OMEGALUL ')
+    render :json => { "message": url }
   end
 end
